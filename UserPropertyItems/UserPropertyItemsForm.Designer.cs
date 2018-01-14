1bcfcd470d2c9f51a43ecbf05904570a416631bc
Cory LePage <cory@razorcx.com>
2018-01-13 16:01:34 -0800

    Initial commit

---
+++ UserPropertyItems/UserPropertyItemsForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace UserPropertyItems
+{
+	partial class UserPropertyItemsForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserPropertyItemsForm));
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.buttonGO = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(12, 101);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(1118, 288);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::UserPropertyItems.Properties.Resources.Logo;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(160, 41);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 6;
+			this.pictureBox1.TabStop = false;
+			// 
+			// buttonGO
+			// 
+			this.buttonGO.Location = new System.Drawing.Point(969, 54);
+			this.buttonGO.Name = "buttonGO";
+			this.buttonGO.Size = new System.Drawing.Size(161, 41);
+			this.buttonGO.TabIndex = 7;
+			this.buttonGO.Text = "All Beams";
+			this.buttonGO.UseVisualStyleBackColor = true;
+			this.buttonGO.Click += new System.EventHandler(this.buttonGO_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 78);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(167, 17);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "RazorCX User Properties";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(802, 54);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(161, 41);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "Select Beams";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// UserPropertyItemsForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1142, 401);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.buttonGO);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.dataGridView1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "UserPropertyItemsForm";
+			this.Text = "User Property Item Example";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button buttonGO;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ UserPropertyItems/UserPropertyItemsForm.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using MoreLinq;
+using Tekla.Structures;
+using Tekla.Structures.Catalogs;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
+
+namespace UserPropertyItems
+{
+	public partial class UserPropertyItemsForm : Form
+	{
+		private readonly Model _model = new Model();
+
+		public UserPropertyItemsForm()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonGO_Click(object sender, EventArgs e)
+		{
+			//get all model objects of type BEAM  (columns removed)
+			ModelObjectEnumerator.AutoFetch = true;
+			var beams = _model.GetAllBeams()
+				.Where(b => b.Type == Beam.BeamTypeEnum.BEAM)
+				.ToList();
+
+			Process(beams);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var beams = _model.PickMultipleObjects()
+				.OfType<Beam>()
+				.Where(b => b.Type == Beam.BeamTypeEnum.BEAM)
+				.ToList();
+
+			Process(beams);
+		}
+
+		private void Process(List<Beam> beams)
+		{
+			//create list of custom user properties
+			var names = new List<string>
+			{
+				"RAZORCX_TIMESTAMP",
+				"RAZORCX_1",
+				"RAZORCX_2",
+				"RAZORCX_3",
+				"RAZORCX_4"
+			};
+
+			//create the custom user properties for model objects of type BEAM
+			CreateUserPropertyItem(names);
+
+			//set user properties (could be anything, we are using beam properties for example)
+			beams.ForEach(b =>
+			{
+				b.SetUserProperty("RAZORCX_TIMESTAMP", DateTime.UtcNow.ToString());
+				b.SetUserProperty("RAZORCX_1", $"{b.Name} {b.Profile.ProfileString} {b.Material.MaterialString}");
+				b.SetUserProperty("RAZORCX_2", $"{b.Profile.ProfileString}");
+				b.SetUserProperty("RAZORCX_3", $"{b.Material.MaterialString}");
+				b.SetUserProperty("RAZORCX_4", $"{b.Finish}");
+			});
+
+			//get user property item values and create anonymous list as datasource for datagridview
+			var userPropertyItems = beams.Select(b =>
+			{
+				var values = new Hashtable();
+				b.GetAllUserProperties(ref values);
+
+				return new
+				{
+					RAZORCX_TIMESTAMP = values[names[0]],
+					RAZORCX_1 = values[names[1]],
+					RAZORCX_2 = values[names[2]],
+					RAZORCX_3 = values[names[3]],
+					RAZORCX_4 = values[names[4]],
+				};
+			}).ToList();
+
+			dataGridView1.DataSource = userPropertyItems;
+
+			//get user properties and output results to console
+			beams.ForEach(b =>
+			{
+				var value = string.Empty;
+				b.GetUserProperty("RAZORCX_TIMESTAMP", ref value);
+
+				var values = new Hashtable();
+				b.GetAllUserProperties(ref values);
+
+				Console.WriteLine(value);
+
+				values.OfType<DictionaryEntry>().ForEach(pair =>
+				{
+					Console.WriteLine($@"{pair.Key}: {values[pair.Key]}");
+				});
+
+				Console.WriteLine("------------------------------------------");
+			});
+		}
+
+		private void CreateUserPropertyItem(List<string> names)
+		{
+			names.ForEach(name =>
+			{
+				var up = new UserPropertyItem
+				{
+					Name = name,
+					Level = UserPropertyLevelEnum.LEVEL_MODEL,
+					FieldType = UserPropertyFieldTypeEnum.FIELDTYPE_TEXT,
+					Type = PropertyTypeEnum.TYPE_STRING,
+					Visibility = UserPropertyVisibilityEnum.VISIBILITY_READONLY,
+					Unique = true,
+					AffectsNumbering = true
+				};
+				up.Insert();
+				up.SetLabel(name);
+				up.AddToObjectType(CatalogObjectTypeEnum.STEEL_BEAM);
+			});
+		}
+	}
+
+	public static class ExtentionMethods
+	{
+		public static List<Beam> GetAllBeams(this Model model)
+		{
+			return model.GetModelObjectSelector()
+				.GetAllObjectsWithType(ModelObject.ModelObjectEnum.BEAM)
+				.ToAList<Beam>();
+		}
+
+		public static List<T> ToAList<T>(this IEnumerator enumerator)
+		{
+			var list = new List<T>();
+			while (enumerator.MoveNext())
+			{
+				var current = (T)enumerator.Current;
+				if (current != null)
+					list.Add(current);
+			}
+			return list;
+		}
+
+		public static List<ModelObject> PickMultipleObjects(this Model model)
+		{
+			ModelObjectEnumerator.AutoFetch = true;
+
+			try
+			{
+				var p = new Picker();
+				var picked =
+					p.PickObjects(Picker.PickObjectsEnum.PICK_N_OBJECTS,
+						"Pick objects (single or multiple)")
+						.ToAList<ModelObject>();
+				return
+					picked.GroupBy(c => c.Identifier.ID)
+						.Select(g => g.FirstOrDefault())
+						.ToList();
+			}
+			catch
+			{
+				return null;
+			}
+		}
+	}
+}
